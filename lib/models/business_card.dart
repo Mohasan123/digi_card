dc8682eb7154de7392f4827504f898ddf91e316d
moham <mohamedismailhamdani@gmail.com>
2024-08-01 18:33:45 +0100

    cam Scanner code

---
+++ lib/models/business_card.dart
@@ -0,0 +1,13 @@
+class BusinessCard {
+  String fullName;
+  String jobTitle;
+  String email;
+  String location;
+
+  BusinessCard({
+    this.fullName = '',
+    this.jobTitle = '',
+    this.email = '',
+    this.location = '',
+  });
+}
